aa495f9079fd56024d5cac7bb8ddd68722db6bbd
Patrick Doherty <patrick.e.doherty@gmail.com>
2018-09-17 17:50:58 +0000

    Contact form, emailjs now working on site

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,9 @@
 function sendMail(contactForm) {
-    emailjs.sendForm("gmail", "rosie", '#contactForm')
+    emailjs.send("gmail", "rosie", {
+        "from_name" : contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
     .then(
         function(response) {
             console.log("SUCCESS", response);
